e6ac24c8c31d75c945dd7e44a1fbe64ba71f652c
Patrick Sodré <psodre@gmail.com>
2021-11-04 21:37:54 -0400

    Add entrypoint that checks for nvidia

--- examples/sagemaker/container/entrypoint.sh
+++ examples/sagemaker/container/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 source /opt/conda/etc/profile.d/conda.sh
-conda activate cpu
+conda activate base
+conda_environment=$(python -c '
+from pynvml import *
+try:
+  nvmlInit()
+  if nvmlDeviceGetCount() > 0:
+    print("gpu")
+except:
+  print("cpu")
+')
+conda activate $conda_environment
 exec "$@"
